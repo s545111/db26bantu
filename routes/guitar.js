1d83f4a10740eb6b27c6c135b06419dbb3631c14
varunbantu <89408502+varunbantu@users.noreply.github.com>
2021-11-10 22:52:18 -0600

    added resource.js

--- routes/guitar.js
+++ routes/guitar.js
@@ -1,5 +1,11 @@
 var express = require('express');
+const guitar_controlers= require('../controllers/guitar');
 var router = express.Router();
+
+/* GET bottle */
+
+router.get('/', guitar_controlers.guitar_view_all_Page );
+module.exports = router;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
