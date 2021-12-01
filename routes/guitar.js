7ef615db5ab9a90ecdd326c1dac583c36b3fb191
varunbantu <89408502+varunbantu@users.noreply.github.com>
2021-11-30 18:08:14 -0600

    added bonus part

--- routes/guitar.js
+++ routes/guitar.js
@@ -13,12 +13,12 @@ const secured = (req, res, next) => {
 router.get('/', guitar_controlers.guitar_view_all_Page );
 router.get('/guitar/:id', guitar_controlers.guitar_detail);
 /* GET detail Guitar page */ 
-router.get('/detail', guitar_controlers.guitar_view_one_Page);
+router.get('/detail',secured, guitar_controlers.guitar_view_one_Page);
 /* GET create Guitar page */ 
 router.get('/create', guitar_controlers.guitar_create_Page);
 /* GET create update page */ 
 router.get('/update',secured, guitar_controlers.guitar_update_Page); 
 /* GET create costume page */ 
-router.get('/delete', guitar_controlers.guitar_delete_Page); 
+router.get('/delete',secured, guitar_controlers.guitar_delete_Page); 
 module.exports = router;
 
